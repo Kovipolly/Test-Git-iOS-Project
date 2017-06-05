662f0abf83efe0e3eb47d07610c2359d710f8471
Kovipolly <kovipolly@gmail.com>
2017-06-06 01:45:08 +0200

    added a test class

---
+++ Git test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git test
+//
+//  Created by Kovacs, Roland on 06/06/17.
+//  Copyright © 2017 Kovacs, Roland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git test
+//
+//  Created by Kovacs, Roland on 06/06/17.
+//  Copyright © 2017 Kovacs, Roland. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
